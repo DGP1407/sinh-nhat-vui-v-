a9e5451ca5deef6d7187b6e3599d7d34d5e43b6c
Duong Gia Phu <Duonggiaphu09@gmail.com>
2022-07-02 13:05:24 +0700

    Happy birthday

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Hiểu Lam';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Sinh Nhật ';
+const titleCard = 'Tặng my babi';
+const contentCard = 'Chúc bé của anh một ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Anh yêu em <3';
+const giftImage = 'bbi.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
